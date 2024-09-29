bacdaa65b4818551771e1743d611262ab4ec6cee
Philipp Stephan <p.stephan@mediaire.de>
2024-09-30 00:36:49 +0200

    triangle and vertex shader mwe

--- fractal.py
+++ fractal.py
@@ -2,19 +2,31 @@
 """https://github.com/pyimgui/pyimgui/blob/master/doc/examples/integrations_glfw3.py"""
 import sys
 
+from OpenGL.raw.GL.VERSION.GL_2_0 import glUseProgram
 from imgui.integrations.glfw import GlfwRenderer
 import OpenGL.GL as gl
 import glfw
 import imgui
+import numpy as np
 
-frag_shader_source = """\
-#version 410
-out vec4 fragColor;
+# Vertex Shader (GLSL)
+vertex_shader_src = """
+#version 330 core
+layout(location = 0) in vec3 aPos;
 void main() {
-	fragColor = vec4(1., 0.5, 0.1, 1.0);
+    gl_Position = vec4(aPos, 1.0);
 }
 """
 
+# Fragment Shader (GLSL)
+fragment_shader_src = """
+#version 330 core
+out vec4 FragColor;
+void main() {
+    vec2 pos = gl_FragCoord.xy / vec2(1280., 720.);
+    FragColor = vec4(pos.x, pos.y, 1., 1.0);
+}
+"""
 
 def main():
     imgui.create_context()
@@ -25,51 +37,49 @@ def main():
 
     values = (1., 1., 1.,)
 
-    init_opengl()
+    VAO, VBO = create_vertices()
+    vertex_shader = create_shader(gl.GL_VERTEX_SHADER, vertex_shader_src)
+    fragment_shader = create_shader(gl.GL_FRAGMENT_SHADER, fragment_shader_src)
+    shader_program = create_program(vertex_shader, fragment_shader)
+    # TODO delete shaders gl.glDeleteShader(fragment_shader)
 
     while not glfw.window_should_close(window):
         glfw.poll_events()
-        impl.process_inputs()
+        # impl.process_inputs()
 
-        imgui.new_frame()
+        # imgui.new_frame()
 
         ######
 
         # open new window context
-        imgui.begin("Your first window!", True)
-        # draw text label inside of current window
-        imgui.text("Hello world!")
-        changed, values = imgui.drag_float3("Drag Float", *values,
-                                            min_value=0.,
-                                            max_value=1.,
-                                            change_speed=0.01)
-        imgui.end()
+        # imgui.begin("Your first window!", True)
+        # # draw text label inside of current window
+        # imgui.text("Hello world!")
+        # changed, values = imgui.drag_float3("Drag Float", *values,
+        #                                     min_value=0.,
+        #                                     max_value=1.,
+        #                                     change_speed=0.01)
+        # imgui.end()
 
         ######
 
-        imgui.render()
-        imgui.end_frame()
-
-
-
-
-
-        #gl.glClearColor(*values, 1)
-        #gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+        # imgui.render()
+        # imgui.end_frame()
 
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        gl.glBegin(gl.GL_TRIANGLE_STRIP)
-        cx, cy = center = 0.5, 0.
-        scale = 1
-        for x in [-1, 1]:
-            for y in [-1, 1]:
-                gl.glTexCoord(x/scale-cx, y/scale-cy)
-                gl.glVertex(x, y)
-        #gl.glEnd()
 
-        imgui.render()
-        impl.render(imgui.get_draw_data())
+        gl.glUseProgram(shader_program)
+        gl.glBindVertexArray(VAO)
+        gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3)
+
+        # imgui.render()
+        # impl.render(imgui.get_draw_data())
         glfw.swap_buffers(window)
+
+    # TODO
+    # glDeleteVertexArrays(1, [VAO])
+    # glDeleteBuffers(1, [VBO])
+    # glDeleteProgram(shader_program)
 
     impl.shutdown()
     glfw.terminate()
@@ -102,29 +112,41 @@ def impl_glfw_init():
     return window
 
 
-def init_opengl():
-	frag_shader = create_shader(frag_shader_source, gl.GL_FRAGMENT_SHADER)
-	program = create_program(frag_shader)
-	gl.glUseProgram(program)
-
-    #gl.glEnable(gl.GL_TEXTURE_1D)
-	#gl.glActiveTexture(gl.GL_TEXTURE0+0)
-	#gl.glBindTexture(gl.GL_TEXTURE_1D, gl.glGenTextures(1))
-	#gl.glTexParameteri(gl.GL_TEXTURE_1D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
-	#gl.glTexParameteri(gl.GL_TEXTURE_1D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
-
-	#gl.glTexImage1D(GL_TEXTURE_1D, 0, GL_LUMINANCE, 16, 0,
-	#             GL_LUMINANCE, GL_UNSIGNED_BYTE,
-	#             "".join(chr(c*16) for c in range(16)))
-
-
 class ShaderCompileError(RuntimeError):
 	"""shader compile error."""
-	
+
 class ProgramLinkError(RuntimeError):
+
 	"""program link error."""
 
-def create_shader(shader_source, shader_type):
+def create_vertices():
+    # Vertex data (triangle)
+    vertices = np.array([
+        [-0.5, -0.5, 0.0],
+        [ 0.5, -0.5, 0.0],
+        [ 0.0,  0.5, 0.0]
+    ], dtype=np.float32)
+
+    # Create Vertex Array Object and Vertex Buffer Object
+    VAO = gl.glGenVertexArrays(1)
+    VBO = gl.glGenBuffers(1)
+
+    gl.glBindVertexArray(VAO)
+
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, VBO)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices.nbytes, vertices, gl.GL_STATIC_DRAW)
+
+    # Specify the layout of the vertex data
+    gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 3 * 4, gl.ctypes.c_void_p(0))
+    gl.glEnableVertexAttribArray(0)
+
+    # Unbind the VAO
+    gl.glBindVertexArray(0)
+
+    return VAO, VBO
+
+
+def create_shader(shader_type, shader_source):
 	"""creates a shader from its source & type."""
 	shader = gl.glCreateShader(shader_type)
 	gl.glShaderSource(shader, shader_source)
@@ -132,6 +154,7 @@ def create_shader(shader_source, shader_type):
 	if gl.glGetShaderiv(shader, gl.GL_COMPILE_STATUS) != gl.GL_TRUE:
 		raise ShaderCompileError(gl.glGetShaderInfoLog(shader))
 	return shader
+
 
 def create_program(*shaders):
 	"""creates a program from its vertex & fragment shader sources."""
