7e60e4972f63e263cf7d5cc10e3e0bb0a986a1a2
Philipp Stephan <p.stephan@mediaire.de>
2024-11-30 18:24:33 +0100

    little smoothing

--- vhsh.py
+++ vhsh.py
@@ -68,9 +68,9 @@ class Microphone(Thread):
                  rate: int = 44100,
                  chunk: int = 1024,
                  buffer_size_s: float = 5.0,
+                 smooth: float = 0.05,
                  format: int | None = None,
-                 intervals: Iterable[int] =
-                    [0, 60, 250, 500, 2_000, 6_000, 8_000]):
+                 intervals: list[int] = [0, 60, 250, 500, 2_000, 6_000, 8_000]):
         super().__init__()
 
         import pyaudio
@@ -81,28 +81,23 @@ class Microphone(Thread):
         self.channels = 1  # TODO configurable?
 
         self._bins = list(zip(intervals, intervals[1:]))
-        self._levels = len(intervals) * (0.0,)  # last interval is open
-
-        self._max_vol = chunk * (2**16-1)  # dynamic
-        self._max = deque(len(intervals) * [self._max_vol],  # last interval is open
-                          maxlen=int(buffer_size_s))
-        self.max = self._max_vol
+        self._levels = deque([np.zeros(len(intervals))],  # last interval is open
+                             maxlen=int(rate/chunk*smooth))
+        self._max_vol = chunk * (2**16-1)  # TODO sizeof(format)
 
         self._frame_buffer = deque(maxlen=int(rate / chunk * buffer_size_s))
         self._stop_stream = Event()
         self._output_lock = Lock()
 
     @property
-    def levels(self) -> tuple[float, ...]:
+    def levels(self) -> list[float]:
         with self._output_lock:
-            return self._levels
-
-    @levels.setter
-    def levels(self, levels: tuple[float, ...]):
-        with self._output_lock:
-            if not isinstance(levels, tuple) or len(levels) != len(self._levels):
-                raise ValueError(f"levels needs to be a tuple of length {len(self._levels)}")
-            self._levels = levels
+            level_history = np.array(self._levels)
+        max_ = min(self._max_vol, level_history.ravel().max())
+        levels = (level_history.mean(axis=0) / max_
+                  if max_ != 0
+                  else np.zeros(level_history.shape[-1]))
+        return levels.tolist()
 
     def run(self):
         import pyaudio
@@ -139,11 +134,8 @@ class Microphone(Thread):
                 levels = [np.sum(spectrum[(frequencies >= min_) & (frequencies < max_)])
                           for min_, max_ in self._bins]
                 levels.append(np.sum(spectrum[frequencies >= self._bins[-1][1]]))
-                # TODO running average normalization
-                self._max.append(max(levels))
-                self.max = max(self._max)
-                self.levels = tuple(float(l/(min(self.max, self._max_vol)))
-                                    for l in levels)
+                with self._output_lock:
+                    self._levels.append(np.array(levels))
 
         finally:
             stream.stop_stream()
